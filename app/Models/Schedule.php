3481ee7645505078b1130ac92db952365b7b15c0
Jacob Landry <landry.jacob@gmail.com>
2021-09-15 02:08:35 +0000

    silly me, i had already sorted, but forgotten saturday, thinking i hadn't done it at all

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -19,15 +19,10 @@ class Schedule extends Model
     {
         return $this->events()->get()
             ->groupBy('day_of_week')
-            ->sortBy(function($rows, $key) {
-                $order = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-                return array_search($key, $order);
-            })
             ->map(function($events, $key) {
                 return $events->sortBy('start_time');
             })->sortBy(function($events, $key) {
-                return array_search($key, ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']);
+                return array_search($key, ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
             });
     }
 }
