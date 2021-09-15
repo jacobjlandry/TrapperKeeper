3d989d96af272a36fb98a6ff4aa49452ea0aaebc
Jacob Landry <jacoblandry@Jacobs-MacBook-Pro-3.local>
2021-09-14 21:01:45 -0400

    fixing urls since we're using nginx to route urls right now, everything still thinks its in localhost on the server so we need to fix that before we can use url() or route()

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -10,7 +10,7 @@
     </div>
     <div class="flex flex-col">
         @foreach($schedules as $schedule)
-            <a href="{{ route('schedule.show', ['schedule' => $schedule->id ]) }}">
+            <a href="/schedule/{{ $schedule->id }}">
                 <div class="border-2 rounded border-yellow-300 text-yellow-300 p-4 m-2 hover:border-yellow-600 hover:text-yellow-600 flex justify-between">
                     <div class="">
                         {{ $schedule->name }}
