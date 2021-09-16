7cf411adfdc8fbf1f8708431b9078c471563e4ff
Jacob Landry <jacoblandry@Jacobs-MacBook-Pro-3.local>
2021-09-15 22:47:01 -0400

    fixing layout issues with height on phones

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full w-full bg-black">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="w-full bg-black">
 <head>
     @include('layout.header')
 </head>
-<body class="antialiased w-full h-full" style="background-image: url('/images/tower.jpeg'); background-repeat: repeat;">
+<body class="antialiased w-full" style="background-image: url('/images/tower.jpeg'); background-repeat: repeat;">
     @if(!request()->has('focus'))
         <div class="w-full h-24 bg-black text-gray-300 text-4xl font-sans p-4 border-b-4 border-yellow-700 flex justify-center items-center">
             @yield('title')
