7e8589272070dafcc0d114f04a62ab6a35075c36
Jacob Landry <jacoblandry@Jacobs-MacBook-Pro-3.local>
2021-09-15 21:13:52 -0400

    cleaner looking highlights

--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('body')
-    <div class="flex flex-row">
+    <div class="flex flex-row:x">
         @foreach($schedule->getEventsByWeekday() as $dayOfWeek => $events)
             <div class="border-2 border-yellow-300 rounded m-2 w-96">
                 <div class="text-2xl border-b-2 border-yellow-300 p-4">
@@ -13,14 +13,14 @@
                 </div>
                 <div class="w-full">
                     @foreach($events as $event)
-                        <div class="@if(!$loop->last) border-b-2 border-gray-500 @endif w-full @if(date('D') == substr($dayOfWeek, 0, 3) && strtotime($now) >= strtotime($event->start_time) && strtotime($now) < strtotime($event->end_time)) bg-yellow-600  @endif p-2">
-                            <div class="@if(date('D') == strtolower(substr($dayOfWeek, 0, 3)) && strtotime($now) >= strtotime($event->start_time) && strtotime($now) < strtotime($event->end_time)) text-yellow-900 @else text-gray-500 @endif mb-4">
+                        <div class="@if(!$loop->last) border-b-2 border-gray-500 @endif w-full p-2">
+                            <div class="text-gray-500 mb-4">
                                 {{ $event->start_time }}
                             </div>
-                            <div class="text-lg font-bold @if(date('D') == substr($dayOfWeek, 0, 3) && strtotime($now) >= strtotime($event->start_time) && strtotime($now) < strtotime($event->end_time)) text-yellow-800 @else text-white @endif">
+                            <div class="text-lg font-bold @if(date('D') == substr($dayOfWeek, 0, 3) && strtotime($now) >= strtotime($event->start_time) && strtotime($now) < strtotime($event->end_time)) text-yellow-300 @else text-white @endif">
                                 {{ $event->name }}
                             </div>
-                            <div class="text-base mb-4 @if(date('D') == substr($dayOfWeek, 0, 3) && strtotime($now) >= strtotime($event->start_time) && strtotime($now) < strtotime($event->end_time)) text-yellow-900 @else text-white @endif">
+                            <div class="text-base mb-4 @if(date('D') == substr($dayOfWeek, 0, 3) && strtotime($now) >= strtotime($event->start_time) && strtotime($now) < strtotime($event->end_time)) text-yellow-300 @else text-white @endif">
                                 @foreach(explode(",", $event->description) as $item)
                                     {{ $item }}<br />
                                 @endforeach
