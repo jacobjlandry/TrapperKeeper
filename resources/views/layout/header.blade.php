1f8b0ef7295aac104e286c76c23f09978af4f2b1
Jacob Landry <landry.jacob@gmail.com>
2022-05-24 08:48:11 -0400

    adding apple mobile friendly meta tag to make this function like an app on ios

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="apple-mobile-web-app-capable" content="yes">
 <link href="/css/app.css" rel="stylesheet">
 
 <title>TrapperKeeper</title>
